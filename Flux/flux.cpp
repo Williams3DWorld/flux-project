9658b0e3583d191527d3cba34bd74fcf50a644ab
William Thomas Wheate <williamtwheate@hotmail.com>
2025-09-16 23:40:25 +0100

    feat: setting up SDL libraries

--- Flux/flux.cpp
+++ Flux/flux.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "flux.h"
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_mixer/SDL_mixer.h>
 
-void flux::init() {
-    std::cout<<"successfully initialised flux!"<<std::endl;
+namespace flux {
+    void init() {
+        SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+
+        std::cout<<"successfully initialised flux!"<<std::endl;
+    }
 }
