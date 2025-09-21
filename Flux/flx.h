7055c994e8708834b5a5087b1f0f4619cf59a475
William Thomas Wheate <williamtwheate@hotmail.com>
2025-09-21 16:39:47 +0100

    fix: format fix

--- Flux/flx.h
+++ Flux/flx.h
@@ -3,7 +3,7 @@
 #ifndef FLX_H
 #define FLX_H
 
-#include"flx_instance.h"
+#include "flx_instance.h"
 
 inline FLX_Instance FLX_CreateInstance(const FLX_InstanceOptions &options) {
     return FLX_Instance(options);
