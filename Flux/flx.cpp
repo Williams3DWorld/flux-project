d33e3987dae50bdbb6b3ed189a3b1e49a6903894
Charlie Thomas Peter Wheate <charliewheate42@gmail.com>
2025-09-21 17:09:45 +0100

    refactor: removed redundant includes

--- Flux/flx.cpp
+++ Flux/flx.cpp
@@ -1,6 +1,1 @@
-#include <iostream>
-
 #include "flx.h"
-
-//#include <SDL3_image/SDL_image.h>
-//#include <SDL3_mixer/SDL_mixer.h>
--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,8 +1,8 @@
-﻿#include <iostream>
-#include <flx.h>
+﻿#include <flx.h>
 
 int main(int argc, char** argv) {
 	FLX_Init();
+
 	const FLX_Instance instance = FLX_CreateInstance({});
 
 	instance.run();
