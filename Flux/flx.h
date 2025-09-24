da74354910f4b7727393765d34b50324d8e8e8b6
William Thomas Wheate <williamtwheate@hotmail.com>
2025-09-24 21:38:44 +0100

    feat: adding memory functions

--- Flux/flx.h
+++ Flux/flx.h
@@ -4,6 +4,7 @@
 #define FLX_H
 
 #include "flx_instance.h"
+#include "flx_memory.h"
 
 FLX_Instance FLX_CreateInstance(const FLX_InstanceOptions &options);
 
--- Flux/flx_memory.h
+++ Flux/flx_memory.h
@@ -6,9 +6,22 @@
 #include <memory>
 
 template<typename T>
+using flx_weak = std::weak_ptr<T>;
+
+template<typename T>
 using flx_unique = std::unique_ptr<T>;
 
 template<typename T>
-using flx_weak = std::weak_ptr<T>;
+using flx_shared = std::shared_ptr<T>;
+
+template<typename T, typename... Args>
+flx_shared<T> flx_make_shared(Args&&... args) {
+    return std::make_shared<T>(args...);
+}
+
+template<typename T, typename... Args>
+flx_unique<T> flx_make_unique(Args&&... args) {
+    return std::make_unique<T>(args...);
+}
 
 #endif
