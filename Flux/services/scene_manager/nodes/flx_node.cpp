97b4c5623917ac53fa16b1dec1ef3742c1981bbe
Charlie Thomas Peter Wheate <charliewheate42@gmail.com>
2025-12-23 22:03:56 +0000

    feat: support for removing children

--- Flux/services/scene_manager/nodes/flx_node.cpp
+++ Flux/services/scene_manager/nodes/flx_node.cpp
@@ -1,16 +1,38 @@
 #include "flx_node.h"
 
 void FLX_Node::addChild(std::unique_ptr<FLX_Node> child) {
-    child->parent = this;
+    child->setParent(this);
     _children.emplace_back(std::move(child));
 }
 
-void FLX_Node::removeChild(const std::unique_ptr<FLX_Node>& child) {
-    std::erase(_children, child);
+void FLX_Node::removeChild(FLX_Node* child) {
+    const auto it = std::remove_if(
+      _children.begin(),
+      _children.end(),
+      [&](const std::unique_ptr<FLX_Node>& ptr) {
+        return ptr.get() == child;
+      }
+    );
+    if (it != _children.end()) {
+        child->setParent(nullptr);
+        _children.erase(it, _children.end());
+    }
+}
+
+void FLX_Node::removeAllChildren() {
+    _children.clear();
+}
+
+void FLX_Node::setParent(FLX_Node* parent_node) {
+    _parent = parent_node;
+}
+
+void FLX_Node::setEnabled(const bool value) {
+    _enabled = value;
 }
 
 void FLX_Node::update(float deltaTime) const {
-    for (auto& child : _children) {
+    for (const auto& child : _children) {
         child->update(deltaTime);
     }
 }
--- Flux/services/scene_manager/nodes/flx_node.h
+++ Flux/services/scene_manager/nodes/flx_node.h
@@ -9,19 +9,23 @@
 class FLX_Node {
 public:
     FLX_Node() = default;
-    ~FLX_Node() = default;
+    virtual ~FLX_Node() = default;
 
+    void setEnabled(bool value);
+    void setParent(FLX_Node* parent_node);
     void addChild(std::unique_ptr<FLX_Node> child);
-    void removeChild(const std::unique_ptr<FLX_Node>& child);
+    void removeChild(FLX_Node* child);
+    void removeAllChildren();
 
-    [[nodiscard]] FLX_Node* getParent() const { return parent; }
-    [[nodiscard]] std::vector<std::unique_ptr<FLX_Node>> getChildren() const { return _children; }
+    [[nodiscard]] FLX_Node* getParent() const { return _parent; }
+    [[nodiscard]] const std::vector<std::unique_ptr<FLX_Node>>& getChildren() const { return _children; }
 
     void update(float deltaTime) const;
     void render() const {}
 private:
+    bool _enabled = true;
     std::vector<std::unique_ptr<FLX_Node>> _children;
-    FLX_Node* parent = nullptr;
+    FLX_Node* _parent = nullptr;
 };
 
 #endif
